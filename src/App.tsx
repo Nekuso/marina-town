7452a9d56038272b020a464aa88c2597cff38e10
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-07-31 19:09:46 +0800

    Added files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <BrowserRouter>
+        <Navbar/>
+
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homepage() {
+    return (
+    <div className="homepage">
+        
+    </div>
+    )
+}
+
+export default Homepage
