f076e7c9b5ce4265a3230a7a2a552f873989f800
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-07-31 19:25:08 +0800

    Added files via upload

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,12 @@ import React from 'react'
 function Navbar() {
   return (
     <div className="navbar__container">
+        <ul className="links__container">
+            <li className="link"><a href="">HOME</a></li>
+            <li className="link"><a href="">FEATURES</a></li>
+            <li className="link"><a href="">SERVICES</a></li>
+            <li className="link"><a href="">ABOUT US</a></li>
+        </ul>
     </div>
   )
 }
