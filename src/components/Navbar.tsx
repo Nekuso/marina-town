fe86417741daf813750a433e1098e7e4aa315fb4
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-07-31 19:26:47 +0800

    Added files via upload

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,10 @@ function Navbar() {
   return (
     <div className="navbar__container">
         <ul className="links__container">
-            <li className="link"><a href="">HOME</a></li>
-            <li className="link"><a href="">FEATURES</a></li>
-            <li className="link"><a href="">SERVICES</a></li>
-            <li className="link"><a href="">ABOUT US</a></li>
+            <li className="link"><a href="/">HOME</a></li>
+            <li className="link"><a href="/">FEATURES</a></li>
+            <li className="link"><a href="/">SERVICES</a></li>
+            <li className="link"><a href="/">ABOUT US</a></li>
         </ul>
     </div>
   )
