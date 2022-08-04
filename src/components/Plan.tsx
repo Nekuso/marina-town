278be8bdd4d34877f2568d3608aa8d1889216fe7
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-08-04 21:06:46 +0800

    Added files via upload

--- src/components/Plan.tsx
+++ src/components/Plan.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { StyledPlan } from './styles/Plan.styled';
+import SitePlan from '../img/SitePlan.png';
 
 function Plan() {
   return (
     <StyledPlan>
         <div className="plan__section">
-
+            <div className="plan__content">
+                <h2>Site Development Plan</h2>
+                <div className="plan__container">
+                    <img src={SitePlan} alt="plan" />
+                    <div className="plan__info">
+                        <p>A well-Planned Community</p>
+                        <p>Situated within the 1.9-hectare Marina Town, Marina Spatial is a modern-contemporary themed community with 3 buildings, each 6 to 10 storeys high.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </StyledPlan>
   )
--- src/components/styles/Plan.styled.tsx
+++ src/components/styles/Plan.styled.tsx
@@ -2,7 +2,22 @@ import styled from 'styled-components';
 
 export const StyledPlan = styled.div`
     .plan__section {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         width: 100vw;
         height: 100vh;
+
+        .plan__content {
+            width: 90vw;
+            height: auto;
+            
+
+            .plan__container {
+                img {
+                    width: 100%;
+                }
+            }
+        }
     }
 `;
