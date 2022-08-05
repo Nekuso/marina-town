7a1a465782e44e54fb68cbb2bad1f83dd0833d66
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-08-05 19:47:43 +0800

    Added files via  upload

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function Form() {
-  return (
-    <div>Form</div>
-  )
+    return (
+        <div className="form__section">
+            
+        </div>
+    )
 }
 
 export default Form
